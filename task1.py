c7b12f4eafdc91eda65a8a4460d943aef9683f26
Oleksandr <96943240+Alex19919@users.noreply.github.com>
2022-07-16 22:30:22 +0300

    task1

--- task1.py
+++ task1.py
@@ -1,1 +1,12 @@
-print('Hello')
+# num = int(input("Enter a number: "))
+# if (num % 2) == 0:
+#    print("{0} Парне".format(num))
+# else:
+#    print("{0} Не парне".format(num))
+
+
+num = 30
+if num % 2 == 0:
+   print('Число парне')
+else:
+   print('Число не парне')
